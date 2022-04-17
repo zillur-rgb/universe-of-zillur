edeb8decc9421b30b0226022dec5a0e3253c96b2
zillur-rgb <zillurdeu@gmail.com>
2022-04-17 20:05:12 +0200

    Initialization of React App

---
+++ developer-portfolio/src/Pages/AllPages/AllPages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const AllPages = () => {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
+};
+
+export default AllPages;
