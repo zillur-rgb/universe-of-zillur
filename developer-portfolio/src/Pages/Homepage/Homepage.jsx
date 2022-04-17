b07babf77235da16d96fd1df74b12b36ed4a5262
zillur-rgb <zillurdeu@gmail.com>
2022-04-18 01:35:30 +0200

    Homepage Done

--- developer-portfolio/src/Pages/Homepage/Homepage.jsx
+++ developer-portfolio/src/Pages/Homepage/Homepage.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import self from "../../assets/imgs/self.svg";
+import "./Homepage.css";
 
 const Homepage = () => {
   return (
-    <div>
+    <div className="homepage-container">
       <Navbar />
+      <div className="resume">
+        <div className="resumeBorder"></div>
+        <p>Download Resume</p>
+      </div>
+      <main className="contents">
+        <div className="details">
+          <h1 className="title">HELLO!</h1>
+          <h1 className="title">This is Zillur.</h1>
+          <p>Frontend Developer & User Interface Designer</p>
+        </div>
+        <div className="image">
+          <div className="border"></div>
+          <img src={self} alt="self" />
+        </div>
+      </main>
     </div>
   );
 };
